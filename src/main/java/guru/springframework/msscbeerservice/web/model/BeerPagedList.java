9c65977501e0bad286cfd96b0f63d919c070621a
Luca Moro <71976553+morodev@users.noreply.github.com>
2021-01-05 11:08:28 +0100

    41. Data Model. Closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * Created by Luca Moro on 05/01/2021 11:04
+ */
+public class BeerPagedList extends PageImpl<BeerDto> {
+
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDto> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * Created by Luca Moro on 05/01/2021
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
