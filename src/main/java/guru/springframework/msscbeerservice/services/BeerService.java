9f240a837683f90b28e645daf2525f3a28d84fcc
Luca Moro <71976553+morodev@users.noreply.github.com>
2021-01-31 18:34:18 +0100

    129. Add show inventory as Parameter and Assignment Review

--- src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
@@ -11,9 +11,9 @@ import java.util.UUID;
  * Created by Luca Moro on 23/01/2021 on 17:31
  */
 public interface BeerService {
-    BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
+    BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand);
 
-    BeerDto getById(UUID beerId);
+    BeerDto getById(UUID beerId, Boolean showInventoryOnHand);
 
     BeerDto saveNewBeer(BeerDto beerDto);
 
